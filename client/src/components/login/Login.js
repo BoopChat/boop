52040ab8645143a3303ffb6d3ba88191ae3bcc80
D-Garnes <dg.projectbuilds@gmail.com>
2021-08-27 13:57:29 -0400

    added comments

--- client/src/components/login/Login.js
+++ client/src/components/login/Login.js
@@ -1,8 +1,14 @@
 import { GoogleLogin } from "react-google-login";
 
 const Login = () => {
+  /*when the user clicks the google sign in button and logs into google, it returns
+  data which contains web token to be verified on the server.
+  */
   const handleLogin = async (googleData) => {
     try {
+      /*Sends the recieved web token to the server to be verified. If verified,
+      some user data is returned e.g username, email, imageUrl.
+      */
       const res = await fetch("http://localhost:8000/auth/google", {
         method: "POST",
         body: JSON.stringify({
@@ -13,9 +19,13 @@ const Login = () => {
         },
       });
 
+      //extract user data from the Object the the fetch command returns
       const user = await res.json();
+
+      //For testing: to be delete.
       console.log(user);
     } catch (err) {
+      //For testing: to be delete.
       console.log(err);
     }
 
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,27 +1,33 @@
 var express = require("express");
 var router = express.Router();
 
+//Google auth client verifies the token sent from the front-end
 const { OAuth2Client } = require("google-auth-library");
 const client = new OAuth2Client(process.env.CLIENT_ID);
 
+//google authentication route
 router.post("/google", async (req, res) => {
+  //stores the token that was sent from the front-ent in the request body.
   const { token } = req.body;
 
   try {
+    //google auth client verifies token and returns a ticket containing user information
     const ticket = await client.verifyIdToken({
       idToken: token,
       audience: process.env.client_ID,
     });
 
+    //extract the relevant user information from the return ticket.
     const { given_name, family_name, email, picture } = ticket.getPayload();
 
-    /* send data to database using UPSERT REQUEST
-    this will create a record in the db or update at existing one and
-    return the record with the generated userID for the db. user id will then
-    be user to setup the session cookie login in the user
+    /*  here is where the returned user information + any other revelent information would
+    be sent to database using an UPSERT REQUEST. TYhe created or updated user record from the datababe is returned and the database generated userId can be then used to 
+    create a session cookie.
     */
 
-    //this is for testing
+    //Here is where the other return data information can be send back to the from end for dispaying names of images.
+
+    //this is for testing : sample return data.
     const user = {
       userId: "aiaosiud87564",
       firstName: given_name,
