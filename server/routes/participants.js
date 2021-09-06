acbec1b9fa0240e60fd38eb572aaf27b177219a3
Krazeelazy <mm.mightyme@gmail.com>
2021-09-06 11:41:23 -0400

    Fixed controller imports in route files

--- server/routes/participants.js
+++ server/routes/participants.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var controller = require("../controllers/participants");
+var controller = require("../controllers/Participants");
 
 // Manage a single user's participation
 router
