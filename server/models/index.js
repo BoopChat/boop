ae2a0f94c7f98ebf3a5bf6f088bffc8cd5094e45
David Fowler <davidfowl@gmail.com>
2021-09-06 06:58:55 -0700

    Apply some small fixes - Fixed migration script by removing extra default value from boolean field - Use path.join to make the path to the config file

--- server/models/index.js
+++ server/models/index.js
@@ -5,7 +5,7 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require(path.join(__dirname, '..', 'config', 'config.json'))[env];
 const db = {};
 
 let sequelize;
@@ -15,8 +15,7 @@ if (config.use_env_variable) {
     sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
-fs
-    .readdirSync(__dirname)
+fs.readdirSync(__dirname)
     .filter(file =>
         (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js')
     )
