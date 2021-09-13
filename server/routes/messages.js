0d49eae735e3715ccea2b1d80b3b649b7c6e53be
Krazeelazy <mm.mightyme@gmail.com>
2021-09-13 09:46:06 -0400

    Fix message router comment style

--- server/routes/messages.js
+++ server/routes/messages.js
@@ -5,7 +5,9 @@ var controller = require("../controllers/Messages");
 // Manage a single user's conversations
 router
     .route("/:user_id/:conversation_id")
-    .get(controller.getMessages)// get all messages from a conversation
-    .post(controller.addMessage)//create a new message in the conversation
+    // get all messages from a conversation
+    .get(controller.getMessages)
+    //create a new message in the conversation
+    .post(controller.addMessage)
 
 module.exports = router;
