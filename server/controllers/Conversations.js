c6cb7ede3dd70e7a935e654bb29da4f15eea76a1
Krazeelazy <mm.mightyme@gmail.com>
2021-09-13 16:49:22 -0400

    Updated controllers to match new model attributes with camel case + commented out problematic transaction usage in conversations controller

--- server/controllers/Conversations.js
+++ server/controllers/Conversations.js
@@ -3,152 +3,151 @@ const Conversation = db.Conversation;
 const Participant = db.Participant;
 const User = db.User;
 
-// module.exports.addConversation = async (req, res) => {
-//     let user_id = req.params.user_id;
-//     // Need to check that user_id belongs to a valid user and
-//     // matches the id of the requesting user
-
-//     // Validate request
-//     if (!user_id) {
-//         res.status(400).send({ msg: "Content can not be empty!" });
-//         return;
-//     }
-
-//     //create the conversation with the input attributes
-//     let conversation = await Conversation.create({
-//         title: req.body.title,
-//         image_url: req.body.image_url,
-//         // if the options were null set their values to false else use the input values
-//         user_editable_image: req.body.user_editable_image==null ? false :  req.body.user_editable_image,
-//         user_editable_title: req.body.user_editable_title==null ? false :  req.body.user_editable_title,
-//     })
-//     //catch any errors
-//     .catch(err => {
-//         res.status(500).send({
-//             msg: err.message || "Some error occurred while creating the Conversation."
-//         });
-//     });
-
-//     // the participants' user_ids will be passed in an array
-//     let participants = req.body.participants;
-
-//     // add user creating convo as admin
-//     Participant.create({
-//         user_id: user_id,
-//         conversation_id: conversation.id,
-//         is_admin: true
-//     })
-
-//     // add all other users in the participants array as participants in the conversation
-//     let is_admin = participants.length == 1;
-//     participants.forEach(async (participant) => {
-//         // create the association between the user and the conversation
-//         Participant.create({
-//             user_id : participant,
-//             conversation_id: conversation.id,
-//             is_admin: is_admin
-//         })
-//     });
-
-//     //return a success message + the newly created conversation
-//     return res.status(201).send({msg:"Conversation successfully created!", conversation});
-// };
-
-
+// create a conversation
 module.exports.addConversation = async (req, res) => {
-    let user_id = req.params.user_id;
-    // Need to check that user_id belongs to a valid user and
+    let userId = req.params.userId;
+    // Need to check that userId belongs to a valid user and
     // matches the id of the requesting user
 
     // Validate request
-    if (!user_id) {
+    if (!userId) {
         res.status(400).send({ msg: "Content can not be empty!" });
         return;
     }
 
-    try {
-        // Managed Transaction
-        let transaction = await db.sequelize.transaction();
-
-        //create the conversation with the input attributes
-        let conversation = await Conversation.create({
-            title: req.body.title,
-            image_url: req.body.image_url,
-            // if the options were null set their values to false else use the input values
-            user_editable_image: req.body.user_editable_image==null ? false :  req.body.user_editable_image,
-            user_editable_title: req.body.user_editable_title==null ? false :  req.body.user_editable_title,
-        }, { transaction });
-
-        // the participants' user_ids will be passed in an array
-        let participants = req.body.participants;
-
-        // add user creating convo as admin
-        await Participant.create({
-            user_id: user_id,
-            conversation_id: conversation.id,
-            is_admin: true
-        }, { transaction });
-
-        // add all other users in the participants array as participants in the conversation
-        let is_admin = participants.length == 1;
-
-        try{
-            let transaction2 = await db.sequelize.transaction(); // Managed Transaction
-            participants.forEach(async (participant) => {
-                // create the association between the user and the conversation
-                await Participant.create({
-                    user_id: participant,
-                    conversation_id: conversation.id,
-                    is_admin: is_admin
-                },
-                { transaction: transaction2}).catch(err => {
-                    // transaction2.rollback()
-                    // transaction.rollback()
-                    return res.status(500).send({
-                        msg:
-                        err.message || "Some error occurred while creating the Conversation."
-                    });
-                });
-            });
-            // everything worked as planned - commit the changes
-            // await transaction2.commit()
-        } catch (err){
-            // await transaction2.rollback()
-            // await transaction.rollback()
-
-            return res.status(500).send({
-                msg:
-                err.message || "Some error occurred while creating the Conversation."
-            });
-        }
-
-        // everything worked as planned - commit the changes
-        // await transaction.commit()
-        //return a success message + the newly created conversation
-        return res.status(201).send({msg:"Conversation successfully created!", conversation});
-    } catch (err){
-        // await transaction.rollback()
-
-        return res.status(500).send({
-            msg:
-            err.message || "Some error occurred while creating the Conversation."
+    // create the conversation with the input attributes
+    let conversation = await Conversation.create({
+        title: req.body.title,
+        imageUrl: req.body.imageUrl,
+        // if the options were null set their values to false else use the input values
+        userEditableImage: req.body.userEditableImage==null ? false :  req.body.userEditableImage,
+        userEditableTitle: req.body.userEditableTitle==null ? false :  req.body.userEditableTitle,
+    }).catch(err => { // catch any errors
+        res.status(500).send({
+            msg: err.message || "Some error occurred while creating the Conversation."
         });
-    }
+    });
+
+    // the participants' userIds will be passed in an array
+    let participants = req.body.participants;
+
+    // add user creating convo as admin
+    Participant.create({
+        userId: userId,
+        conversationId: conversation.id,
+        isAdmin: true
+    });
+
+    // add all other users in the participants array as participants in the conversation
+    let isAdmin = participants.length == 1;
+    participants.forEach(async (participant) => {
+        // create the association between the user and the conversation
+        Participant.create({
+            userId : participant,
+            conversationId: conversation.id,
+            isAdmin: isAdmin
+        });
+    });
+
+    //return a success message + the newly created conversation
+    return res.status(201).send({msg:"Conversation successfully created!", conversation});
 };
 
+// Attempting to create a conversation using transactions
+// module.exports.addConversation = async (req, res) => {
+//     let userId = req.params.userId;
+//     // Need to check that userId belongs to a valid user and
+//     // matches the id of the requesting user
+
+//     // Validate request
+//     if (!userId) {
+//         res.status(400).send({ msg: "Content can not be empty!" });
+//         return;
+//     }
+
+//     try {
+//         // Managed Transaction
+//         let transaction = await db.sequelize.transaction();
+
+//         //create the conversation with the input attributes
+//         let conversation = await Conversation.create({
+//             title: req.body.title,
+//             imageUrl: req.body.imageUrl,
+//             // if the options were null set their values to false else use the input values
+//             userEditableImage: req.body.userEditableImage==null ? false :  req.body.userEditableImage,
+//             userEditableTitle: req.body.userEditableTitle==null ? false :  req.body.userEditableTitle,
+//         }, { transaction });
+
+//         // the participants' userIds will be passed in an array
+//         let participants = req.body.participants;
+
+//         // add user creating convo as admin
+//         await Participant.create({
+//             userId: userId,
+//             conversationId: conversation.id,
+//             isAdmin: true
+//         }, { transaction });
+
+//         // add all other users in the participants array as participants in the conversation
+//         let isAdmin = participants.length == 1;
+
+//         try{
+//             let transaction2 = await db.sequelize.transaction(); // Managed Transaction
+//             participants.forEach(async (participant) => {
+//                 // create the association between the user and the conversation
+//                 await Participant.create({
+//                     userId: participant,
+//                     conversationId: conversation.id,
+//                     isAdmin: isAdmin
+//                 },
+//                 { transaction: transaction2}).catch(err => {
+//                     // transaction2.rollback()
+//                     // transaction.rollback()
+//                     return res.status(500).send({
+//                         msg:
+//                         err.message || "Some error occurred while creating the Conversation."
+//                     });
+//                 });
+//             });
+//             // everything worked as planned - commit the changes
+//             // await transaction2.commit()
+//         } catch (err){
+//             // await transaction2.rollback()
+//             // await transaction.rollback()
+
+//             return res.status(500).send({
+//                 msg:
+//                 err.message || "Some error occurred while creating the Conversation."
+//             });
+//         }
+
+//         // everything worked as planned - commit the changes
+//         // await transaction.commit()
+//         //return a success message + the newly created conversation
+//         return res.status(201).send({msg:"Conversation successfully created!", conversation});
+//     } catch (err){
+//         // await transaction.rollback()
+
+//         return res.status(500).send({
+//             msg:
+//             err.message || "Some error occurred while creating the Conversation."
+//         });
+//     }
+// };
+
 module.exports.getConversations = async (req, res) => {
-    let user_id = req.params.user_id;
-    // Need to check that user_id belongs to a valid user and
+    let userId = req.params.userId;
+    // Need to check that userId belongs to a valid user and
     // matches the id of the requesting user
 
-    //get user and all conversations (where Participants.deleted_at is null)
-    let user = await User.findByPk(user_id,{
+    //get user and all conversations (where Participants.deletedAt is null)
+    let user = await User.findByPk(userId,{
         // get the conversation info
         include: {
             model: Conversation,
             as: "conversationList",
             // specify what atributes you want returned
-            attributes:["id", "title", "image_url"],
+            attributes:["id", "title", "imageUrl"],
             // Prevent the belongs-to-many mapping object (Participant)
             // from being returned
             through: {attributes: []},
@@ -157,7 +156,7 @@ module.exports.getConversations = async (req, res) => {
                 model: User,
                 as: "participants",
                 // specify what atributes you want returned
-                attributes:["display_name"],
+                attributes:["displayName"],
                 // Prevents the entire belongs-to-many mapping object (Participant)
                 // from being returned
                 through: {attributes: []}
@@ -171,76 +170,76 @@ module.exports.getConversations = async (req, res) => {
 
 // remove a user from a conversation
 module.exports.leaveConversation = async (req, res) => {
-    let user_id = req.params.user_id;
-    // get the conversation_id and id of the successor admin (successor_id)) from the request body
-    let conversation_id = req.body.conversation_id;
-    let successor_id = req.body.successor_id;
-    // Need to check that user_id belongs to a valid user and
+    let userId = req.params.userId;
+    // get the conversationId and id of the successor admin (successorId)) from the request body
+    let conversationId = req.body.conversationId;
+    let successorId = req.body.successorId;
+    // Need to check that userId belongs to a valid user and
     // matches the id of the requesting user
 
     // Validate request
-    if (!user_id|| !conversation_id) {
+    if (!userId|| !conversationId) {
         res.status(400).send({
             msg: "Content can not be empty!"
         });
         return;
     }
 
-    // Get the participants' user ids and is_admin values, along with the number of participants
-    let participants_info = await Participant.findAndCountAll({
+    // Get the participants' user ids and isAdmin values, along with the number of participants
+    let participantsInfo = await Participant.findAndCountAll({
         where:{
-            conversation_id: conversation_id
+            conversationId: conversationId
         },
-        attributes:["user_id", "is_admin"],
+        attributes:["userId", "isAdmin"],
         include:{
             model: User,
             as: "participantInfo",
-            attributes: ["display_name"]
+            attributes: ["displayName"]
         }
     });
 
     // The number of participants
-    var participants_count = participants_info.count;
-    // The participants' user ids and is_admin values
-    var participants = participants_info.rows;
+    var participantsCount = participantsInfo.count;
+    // The participants' user ids and isAdmin values
+    var participants = participantsInfo.rows;
     // the number of admins
-    var admins_count = 0;
+    var adminsCount = 0;
     // whether or not the user is a participant
-    var user_is_participant = false;
+    var userIsParticipant = false;
     // whether or not the user is an admin
-    var user_is_admin = false;
+    var userIsAdmin = false;
     // whether or not the successor is a participant
-    var successor_is_participant = false;
+    var successorIsParticipant = false;
 
-    // Get the number of admins (count how many participants have is_admin set to true)
+    // Get the number of admins (count how many participants have isAdmin set to true)
     for (let participant of participants) {
-        if(participant.is_admin == true){
-            admins_count += 1;
+        if(participant.isAdmin == true){
+            adminsCount += 1;
         }
         // check if user is a participant
-        if(participant.user_id == user_id){
-            user_is_participant = true;
-            //if the user's an admin set user_is_admin to true
-            if(participant.is_admin == true){
-                user_is_admin =true;
+        if(participant.userId == userId){
+            userIsParticipant = true;
+            //if the user's an admin set userIsAdmin to true
+            if(participant.isAdmin == true){
+                userIsAdmin =true;
             }
         }
         // check if successor is a participant
-        if(participant.user_id == successor_id){
-            successor_is_participant = true;
+        if(participant.userId == successorId){
+            successorIsParticipant = true;
         }
     }
 
     // if the user isn't a participant return an error message
-    if(!user_is_participant){
+    if(!userIsParticipant){
         return res.status(404).send({msg: "Requesting user is not a participant of the conversation"});
     }
 
     // if this is the only participant delete the conversation, all messages and participants
-    if(participants_count == 1){
-        let deleted_conversation_row = await Conversation.destroy({
+    if(participantsCount == 1){
+        let deletedConversationRow = await Conversation.destroy({
             where:{
-                id: conversation_id
+                id: conversationId
             }
         }).catch(err => {//catch any errors
             res.status(500).send({
@@ -249,7 +248,7 @@ module.exports.leaveConversation = async (req, res) => {
             });
         });
 
-        if(!deleted_conversation_row){
+        if(!deletedConversationRow){
             return res.send({msg:"Conversation couldn't be deleted. Probably didn't exist."});
         }
 
@@ -260,9 +259,9 @@ module.exports.leaveConversation = async (req, res) => {
         });
     }
 
-    if(user_is_admin == true){
+    if(userIsAdmin == true){
         // if this is the last admin and they haven't chosen a successor
-        if(admins_count == 1 && !successor_id){
+        if(adminsCount == 1 && !successorId){
             // return a msg letting the user know they must choose a successor and the list of participants
             return res.send({
                 msg: "You're the only admin. You must choose a successor.",
@@ -271,7 +270,7 @@ module.exports.leaveConversation = async (req, res) => {
         }
 
         // if this is the last admin and they chose a successor that's not a participant
-        if(admins_count == 1 && !successor_is_participant){
+        if(adminsCount == 1 && !successorIsParticipant){
             // return a msg letting the user know they must choose a successor that's a participant
             // and the list of participants
             return res.send({
@@ -281,14 +280,14 @@ module.exports.leaveConversation = async (req, res) => {
         }
 
         // if a successor was chosen
-        if(successor_id){
-            // set successor's is_admin value to true
+        if(successorId){
+            // set successor's isAdmin value to true
             Participant.update({
-                is_admin: true
+                isAdmin: true
             },
             {
                 where:{
-                    user_id: successor_id
+                    userId: successorId
                 }
             }).catch(err => {// catch any errors
                 res.status(500).send({
@@ -298,10 +297,10 @@ module.exports.leaveConversation = async (req, res) => {
         }
     }
 
-    let deleted_participant_row = await Participant.destroy({
+    let deletedParticipantRow = await Participant.destroy({
         where:{
-            user_id : user_id,
-            conversation_id: conversation_id
+            userId : userId,
+            conversationId: conversationId
         }
     }).catch(err => {// catch any errors
         res.status(500).send({
@@ -310,7 +309,7 @@ module.exports.leaveConversation = async (req, res) => {
         });
     });
 
-    if(!deleted_participant_row){
+    if(!deletedParticipantRow){
         return res.send({msg:"User couldn't be removed from conversation. Probably wasn't a participant."});
     }
 
