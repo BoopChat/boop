f0dfebfe7cb8757d942317a1cf20a67ed131da96
D-Garnes <dg.projectbuilds@gmail.com>
2021-10-28 13:29:34 -0400

    fixed Eslint error

--- server/socketio/connection.js
+++ server/socketio/connection.js
@@ -1,28 +1,29 @@
 const logger = require("../logger");
 const jwt = require("jsonwebtoken");
 
-global.io.on("connection", (socket) => {
-    // Authentication middle was for socket io connection
-    io.use((socket, next) => {
-        // extracts authentication token from the socket connection
-        const authToken = socket.handshake.auth.token;
+// Authentication middle was for socket io connection
+global.io.use((socket, next) => {
+    // extracts authentication token from the socket connection
+    const authToken = socket.handshake.auth.token;
 
-        // Verify the token
-        try {
-            // Continue if verified or cancel connection and send error to user
-            const user = jwt.verify(authToken, process.env.TOKEN_SECRET);
-            if (!user) {
-                next(new Error("No user information stored in JWT"));
-            } else {
-                socket.join(user.id);
-                next();
-            }
-        } catch (e) {
-            logger.error(e);
-            next(new Error("Invalid jwt token"));
+    // Verify the token
+    try {
+        // Continue if verified or cancel connection and send error to user
+        const user = jwt.verify(authToken, process.env.TOKEN_SECRET);
+        if (!user) {
+            next(new Error("No user information stored in JWT"));
+        } else {
+            console.log("joining", user.id);
+            socket.join(user.id);
+            next();
         }
-    });
+    } catch (e) {
+        logger.error(e);
+        next(new Error("Invalid jwt token"));
+    }
+});
 
+global.io.on("connection", (socket) => {
     socket.on("joinConversations", (conversationIds) => {
         for (const conversationId of conversationIds) {
             socket.join(conversationId);
