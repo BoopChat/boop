be3ce831d40057643cbf7d5a8755f65534ef0d49
Krazeelazy <mm.mightyme@gmail.com>
2022-03-13 23:22:32 -0400

    fix: if no image is present for a convo in the convo list, 1 from picsum.photos is used

--- client/src/components/messenger/Conversations.js
+++ client/src/components/messenger/Conversations.js
@@ -208,7 +208,7 @@ const Conversations = () => {
                         .sort(sortConversations).map((chat, i) => (
                             <ConversationItem
                                 name={chat.title}
-                                img={chat.imgUrl}
+                                img={chat.imgUrl ?? "https://picsum.photos/400?id=" + chat.title}
                                 lastMsg={chat.lastMsg ?? ""}
                                 lastDate={ConversationsController.evaluateDate(chat.lastDate)}
                                 unread={chat.unread}
