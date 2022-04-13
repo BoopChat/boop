3ff9a0579b5c0da5d8d85a749255c32bd4bce231
Krazeelazy <mm.mightyme@gmail.com>
2022-04-13 11:21:18 -0400

    improvements:     - Added call to script to make yaml secrets to bash script     - Updated README file with command needed to run the bash script

--- deploy-script.bash
+++ deploy-script.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+npm run make-yaml-secrets
 kubectl --kubeconfig="$1" apply -f boop-secret.yaml
 kubectl --kubeconfig="$1" create -f boop-deployment.yaml
 kubectl --kubeconfig="$1" apply -f boop-service.yaml
